334ec56a55b9047e3f9227fa219bef95c8656da4
Matthew A. Obrien <M8HY6@regions.com>
2017-01-18 14:35:15 -0600

    add gulp task on dev branch

--- code.js
+++ code.js
@@ -1,6 +1,14 @@
 var gulp = require('gulp'),
 	connect = require('gulp-connect');
 
+gulp.src('client/js/**/*.js') 
+	.pipe(minify())
+	.pipe(gulp.dest('build'));
+
+gulp.src('client/js/**/*.js', {base: 'client'})
+	.pipe(minify())
+	.pipe(gulp.dest('build'));
+
 gulp.task('webserver', function() {
 	connect.server({
 		livereload: true
