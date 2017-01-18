3a43f9dffd0777dacf55548ca1fa35f61aac05cd
Matthew A. Obrien <M8HY6@regions.com>
2017-01-18 14:37:20 -0600

    added fucntionality in master branch

--- code.js
+++ code.js
@@ -1,6 +1,12 @@
 var gulp = require('gulp'),
 	connect = require('gulp-connect');
 
+glup.src('./client/templates/*.jade')
+	.pipe(jade())
+	.pipe(gulp.dest('./build/templates'))
+	.pipe(minify())
+	.pipe(gulp.dest('./build/minified_templates'));
+
 gulp.task('webserver', function() {
 	connect.server({
 		livereload: true
